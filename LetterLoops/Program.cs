47b125eef1599ae9716b35c65d1f4ecc7c2f82ef
John Thielman <john.thielman@gmail.com>
2020-02-29 13:39:34 -0600

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input:");
+            var answer = Console.ReadLine();
+            var output = new StringBuilder();
+            var count = 0;
+            foreach (char c in answer)
+            {
+                output.Append(Char.ToUpper(c));
+                for (var i = 0; i < count; i++)
+                    output.Append(Char.ToLower(c));
+                count++;
+                if (count < answer.Length)
+                {
+                    output.Append('-');
+                }
+            };
+            Console.WriteLine($"Output: {output.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
